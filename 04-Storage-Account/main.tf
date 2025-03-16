ace1745713f8e0e12d80266943aad7cc743bdafe
Atul Kamble <atul_kamble@hotmail.com>
2025-03-16 09:34:18 +0530

    code

---
+++ 04-Storage-Account/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "cc57cd42-dede-4674-b810-a0fbde41504a"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestoraccount98600"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
